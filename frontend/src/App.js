090d4f28729cfcf4fb87d2e4e52420d9cdd7f283
giovannamascarenhas <giovanna.mascarenhas.1@gmail.com>
2020-03-25 17:52:44 +0000

    Making the register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './global.css';
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
 
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css';
+
+export default function Register() {
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={ logoImg } alt="Be The Hero"/>
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="E02041" />
+                        Voltar para o Logon
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Nome da Ong" />
+                    <input type="email" placeholder="E-mail"/>
+                    <input placeholder="WhatsApp"/>
+
+                    <div className="input-group">
+                        <input placeholder="Cidade"/>
+                        <input placeholder="UF" style={{ width: 80 }} />
+                    </div>
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    );
+}
